a69a44ff25f1c7c5679aeb7ed770485b08678014
Gauri Desai <gaurid380@gmail.com>
2023-03-15 14:41:04 +0530

    first commit

---
+++ emp/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Emp,Testimonial
+# Register your models here.
+
+class EmpAdmin(admin.ModelAdmin):
+    list_display=('name','working','emp_id','phone')
+    list_editable=('working','emp_id')
+    search_fields=('name','phone')
+    list_filter=('working',)
+
+admin.site.register(Emp,EmpAdmin)
+admin.site.register(Testimonial)
